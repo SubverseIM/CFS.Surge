ce909b735fa7a2508ee8663e8f9a95cdff0c5b69
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-11-19 11:05:14 -0600

    Fix: create read-only alternative to `AggregateBuffer`

---
+++ CFS.Surge.Core/Buffers/Implementation/ReadOnlyAggregateBuffer.cs
@@ -0,0 +1,47 @@
+﻿namespace CFS.Surge.Core.Buffers.Implementation
+{
+    public readonly struct ReadOnlyAggregateBuffer<T> : IReadOnlyBuffer<T>
+    {
+        private readonly SortedList<long, IReadOnlyBuffer<T>> bufferList;
+
+        private readonly long totalLength;
+
+        public ReadOnlyAggregateBuffer(params IEnumerable<IReadOnlyBuffer<T>> buffers)
+        {
+            bufferList = new();
+            foreach (IReadOnlyBuffer<T> buffer in buffers)
+            {
+                bufferList.Add(totalLength += buffer.Length, buffer);
+            }
+        }
+
+        public ReadOnlyAggregateBuffer<T> Append(IReadOnlyBuffer<T> buffer)
+        {
+            return new ReadOnlyAggregateBuffer<T>([.. bufferList.Values, buffer]);
+        }
+
+        public T this[long index]
+        {
+            get
+            {
+                if (index < 0 || index >= totalLength)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    int i; long offset = 0;
+                    for (i = 0; i < bufferList.Count; i++) 
+                    {
+                        if (bufferList.GetKeyAtIndex(i) > index) break;
+                        offset = bufferList.GetKeyAtIndex(i);
+                    }
+
+                    return bufferList.GetValueAtIndex(i)[index - offset];
+                }
+            }
+        }
+
+        public long Length => totalLength;
+    }
+}
